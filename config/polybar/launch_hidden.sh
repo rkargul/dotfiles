4e65513a2a542ab9aebf1dad81683a5ad9b4e8d0
Zohar Cochavi <zohar.cochavi@gmail.com>
2021-06-06 12:43:23 +0200

    New rofi look, better startups script, and some fixes in bspwmrc

--- config/polybar/launch_hidden.sh
+++ config/polybar/launch_hidden.sh
@@ -2,7 +2,7 @@
 
 $HOME/.config/polybar/launch.sh $1
 
-until hideIt.sh --class '^polybar$' --direction top --hover
+until hideIt.sh --class '^polybar$' --direction top --hover -s 10
 do
 	sleep 1
 	bspc config top_padding 0

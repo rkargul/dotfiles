5b440fc860d64674f48c968344a9e8b8701ca186
Zohar Cochavi <zohar.cochavi@gmail.com>
2021-06-06 16:01:35 +0200

    LAUNCH HIDDEN WORKS RELIABLY \:\), also new look for polybar! (again)

--- config/polybar/launch_hidden.sh
+++ config/polybar/launch_hidden.sh
@@ -2,8 +2,21 @@
 
 $HOME/.config/polybar/launch.sh $1
 
-until hideIt.sh --class '^polybar$' --direction top --hover -s 10
-do
+sleep 1
+
+# Wait for polybar to be launched
+until [[ $(ps aux | grep polybar) ]]; do
 	sleep 1
-	bspc config top_padding 0
-done 
+done
+
+# Kill al instances of hideIt
+for PID in $(ps aux | grep hideIt.sh | grep -oP '^zohar\s+\K(\w+)\s' | head -n 3); do
+	kill -9 $PID
+done
+
+# Run hideIt
+exec hideIt.sh --class '^polybar$' --direction top -p $2 -H &> /dev/null &
+
+# Remove top padding
+bspc config top_padding $2
+

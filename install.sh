9a27e0f73d42976b5ccb0b34e43177bf00f6fa74
Zohar Cochavi <zohar.cochavi@gmail.com>
2021-06-09 13:47:20 +0200

    Fix style check errors

--- install.sh
+++ install.sh
@@ -4,25 +4,25 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 TARGET_DIR=$1
 
-if [[ $TARGET_DIR == "" ]]; then
+if [[ "$TARGET_DIR" == "" ]]; then
 	echo "No second argument found, defaulting to $HOME"
-	TARGET_DIR=$HOME
+	TARGET_DIR="$HOME"
 fi
 
-for file in $SCRIPT_DIR/dotfiles/.*; do
-	if ! [[ ${file: -1} == "." ]] && ! [[ ${file: -1} == ".." ]]; then
-		echo "Copying $file, to $TARGET_DIR"
-		cp -r $file/ $TARGET_DIR &> /dev/null || cp $file $TARGET_DIR &> /dev/null
+for file in "$SCRIPT_DIR"/dotfiles/.*; do
+	if ! [[ "${file: -1}" == "." ]] && ! [[ "${file: -1}" == ".." ]]; then
+		echo "Copying $file", to "$TARGET_DIR"
+		cp -r "$file"/ "$TARGET_DIR" &> /dev/null || cp "$file" "$TARGET_DIR" &> /dev/null
 	fi
 done
 
-for dir in $SCRIPT_DIR/config/*; do
-	if ! [[ $(ls -al $TARGET_DIR/ | grep .config) ]]; then
+for dir in "$SCRIPT_DIR"/config/*; do
+	if ! [[ $(ls -al "$TARGET_DIR"/ | grep .config) ]]; then
 		echo "Couldn't find .config, creating one..."
-		mkdir $TARGET_DIR/.config
+		mkdir "$TARGET_DIR"/.config
 	fi
-	if ! [[ ${dir: -1} == "." ]] && ! [[ ${dir: -1} == ".." ]]; then
+	if ! [[ "${dir: -1}" == "." ]] && ! [[ ${dir: -1} == ".." ]]; then
 		echo "Copying $dir, to $TARGET_DIR"
-		cp -r $dir/ $TARGET_DIR/.config &> /dev/null
+		cp -r "$dir"/ "$TARGET_DIR"/.config &> /dev/null
 	fi
 done
--- sync.sh
+++ sync.sh
@@ -5,18 +5,16 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 mkdir config &> /dev/null
 mkdir dotfiles &> /dev/null
 
-input=$SCRIPT_DIR/config.txt
+input="$SCRIPT_DIR"/config.txt
 
 while IFS= read -r dir; do
 	echo "Copying $dir"
-	cp -r $HOME/.config/$dir $SCRIPT_DIR/config/ &> /dev/null
-done < $input
+	cp -r "$HOME"/.config/"$dir" "$SCRIPT_DIR"/config/ &> /dev/null
+done < "$input"
 
-
-input=$SCRIPT_DIR/dotfiles.txt
+input="$SCRIPT_DIR"/dotfiles.txt
 
 while IFS= read -r file; do
-	echo "Copying $file"
-	cp -r $HOME/$file $SCRIPT_DIR/dotfiles/ &> /dev/null
-done < $input
-
+	echo "Copying "$file""
+	cp -r $HOME/"$file" "$SCRIPT_DIR"/dotfiles/ &> /dev/null
+done < "$input"

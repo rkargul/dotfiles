e9c33d9c310246d62482ea3c39fa888e19371916
Zohar Cochavi <cochavi.zohar@protonmail.com>
2022-02-02 17:02:52 +0100

    Use symlinks instead of copying back-and-forth (#2)
    * Change bunch of things...
    
    * Reworked dotfiles to use symlinks
    
    Not sure why I didn't do this before...
    
    * Update README.md and small changes to install.sh
    
    * Changes README.md
    
    * Changes README.md, and try fix circleCi issues
    
    * Fixes some bugs
    
    * Make circleCI happy
    
    * Hard one to please I see...

--- install.sh
+++ install.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
 
+cecho() {
+	# shellcheck disable=SC2034
+    RED="\033[0;31m"
+	# shellcheck disable=SC2034
+    GREEN="\033[0;32m"
+	# shellcheck disable=SC2034
+    YELLOW="\033[1;33m"
+	# shellcheck disable=SC2034
+    NC="\033[0m" # No Color
+	# shellcheck disable=SC2059
+    printf "${!1}${2} ${NC}\n"
+}
+
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
+# full directory path into which dotfiles will be linked and the .config folder will reside
+# default: $HOME
 TARGET_DIR=$1
+
+# if last argument set to NUKE it will remove conflicting files found in $TARGET_DIR and $TARGET_DIR/.config if
+# the latter already exists
+NUKE=${*: -1}
 
 # set $TARGET_DIR to default if not set
 if [[ "$TARGET_DIR" == "" ]]; then
@@ -19,24 +38,43 @@ fi
 # copy all dotfiles to $TARGET_DIR
 for file in "$SCRIPT_DIR"/dotfiles/.*; do
 	if ! [[ "${file: -1}" == "." ]] && ! [[ "${file: -1}" == ".." ]]; then
-		echo "Copying $file", to "$TARGET_DIR"
-		cp -r "$file" "$TARGET_DIR"/ &> /dev/null || cp "$file" "$TARGET_DIR" &> /dev/null
+		FILENAME=${file##*/}
+
+		if [ -f "$TARGET_DIR"/"$FILENAME" ] || [ -d "$TARGET_DIR"/"$FILENAME" ]; then
+			if [[ $NUKE == "NUKE" ]]; then
+				rm -r "${TARGET_DIR:?}"/"$FILENAME"
+				cecho "YELLOW" "Found and removed $FILENAME from $TARGET_DIR"
+			fi
+		fi
+
+		if ln -s "$file" "$TARGET_DIR"; then
+			cecho "GREEN" "Linked $file to $TARGET_DIR/$FILENAME"
+		else
+			cecho "RED" "Failed to link $file to $TARGET_DIR/$FILENAME, file exists"
+		fi
 	fi
 done
 
 # if .config doesnt exist, create it
-if [ ! -d "$TARGET_DIR"/.config ]; then
+if [ ! -d "$TARGET_DIR/.config" ]; then
 	echo "Couldn't find .config, creating one..."
-	mkdir -p "$TARGET_DIR"/.config
+	mkdir -p "$TARGET_DIR/.config"
 fi
 
 # copy all dirs in config to $TARGET_DIR/.config
 for dir in "$SCRIPT_DIR"/config/*; do
 	if ! [[ "${dir: -1}" == "." ]] && ! [[ "${dir: -1}" == ".." ]]; then
-		if cp -rf "$dir" "$TARGET_DIR"/.config; then
-			echo "Copying $dir, to $TARGET_DIR"/.config
+		DIRNAME=${dir##*/}
+
+		if [ -d "$TARGET_DIR"/.config/"$DIRNAME" ] && [[ $NUKE == "NUKE" ]]; then
+			rm "$TARGET_DIR"/.config/"$DIRNAME"
+			cecho "YELLOW" "Found and removed $dir from $TARGET_DIR/.config"
+		fi
+
+		if ln -sv "$dir" "$TARGET_DIR"/.config/ &> /dev/null; then
+			cecho "GREEN" "Linked $dir to $TARGET_DIR/.config/$DIRNAME"
 		else
-			echo "Failed to copy $dir, to $TARGET_DIR"/.config
+			cecho "RED" "Failed to link $dir to $TARGET_DIR/.config/$DIRNAME, file exists"
 		fi
 	fi
 done

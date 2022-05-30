b105afcc6488981f2d2a618b23eb59cac7981a3f
Zohar Cochavi <cochavi.zohar@protonmail.com>
2022-05-30 12:00:59 +0200

    Update doom config

--- dotfiles/.doom.d/config.el
+++ dotfiles/.doom.d/config.el
@@ -105,94 +105,7 @@
           (?B . 'all-the-icons-orange)
           (?C . 'all-the-icons-yellow)
           (?D . 'all-the-icons-green)
-          (?E . 'all-the-icons-blue)))
-  ;; Extra ligatures
-;;   (appendq! +ligatures-extra-symbols
-;;             `(:checkbox      "☐"
-;;               :pending       "◼"
-;;               :checkedbox    "☑"
-;;               :list_property "∷"
-;;               :em_dash       "—"
-;;               :ellipses      "…"
-;;               :arrow_right   "→"
-;;               :arrow_left    "←"
-;;               :title         "𝙏"
-;;               :subtitle      "𝙩"
-;;               :author        "𝘼"
-;;               :date          "𝘿"
-;;               :property      "☸"
-;;               :options       "⌥"
-;;               :startup       "⏻"
-;;               :macro         "𝓜"
-;;               :html_head     "🅷"
-;;               :html          "🅗"
-;;               :latex_class   "🄻"
-;;               :latex_header  "🅻"
-;;               :beamer_header "🅑"
-;;               :latex         "🅛"
-;;               :attr_latex    "🄛"
-;;               :attr_html     "🄗"
-;;               :attr_org      "⒪"
-;;               :begin_quote   "❝"
-;;               :end_quote     "❞"
-;;               :caption       "☰"
-;;               :header        "›"
-;;               :results       "🠶"
-;;               :begin_export  "⏩"
-;;               :end_export    "⏪"
-;;               :properties    "⚙"
-;;               :end           "∎"
-;;               :priority_a   ,(propertize "⚑" 'face 'all-the-icons-red)
-;;               :priority_b   ,(propertize "⬆" 'face 'all-the-icons-orange)
-;;               :priority_c   ,(propertize "■" 'face 'all-the-icons-yellow)
-;;               :priority_d   ,(propertize "⬇" 'face 'all-the-icons-green)
-;;               :priority_e   ,(propertize "❓" 'face 'all-the-icons-blue)))
-
-  ;; Extra extra ligatures
-;  (set-ligatures! 'org-mode
-;    :merge t
-;    :checkbox      "[ ]"
-;    :pending       "[-]"
-;    :checkedbox    "[X]"
-;    :list_property "::"
-;    :em_dash       "---"
-;    :ellipsis      "..."
-;    :arrow_right   "->"
-;    :arrow_left    "<-"
-;    :title         "#+title:"
-;    :subtitle      "#+subtitle:"
-;    :author        "#+author:"
-;    :date          "#+date:"
-;    :property      "#+property:"
-;    :options       "#+options:"
-;    :startup       "#+startup:"
-;    :macro         "#+macro:"
-;    :html_head     "#+html_head:"
-;    :html          "#+html:"
-;    :latex_class   "#+latex_class:"
-;    :latex_header  "#+latex_header:"
-;    :beamer_header "#+beamer_header:"
-;    :latex         "#+latex:"
-;    :attr_latex    "#+attr_latex:"
-;    :attr_html     "#+attr_html:"
-;    :attr_org      "#+attr_org:"
-;    :begin_quote   "#+begin_quote"
-;    :end_quote     "#+end_quote"
-;    :caption       "#+caption:"
-;    :header        "#+header:"
-;    :begin_export  "#+begin_export"
-;    :end_export    "#+end_export"
-;    :results       "#+RESULTS:"
-;    :property      ":PROPERTIES:"
-;    :end           ":END:"
-;    :priority_a    "[#A]"
-;    :priority_b    "[#B]"
-;    :priority_c    "[#C]"
-;    :priority_d    "[#D]"
-;    :priority_e    "[#E]")
-;
-;  (plist-put +ligatures-extra-symbols :name "⁍")
-)
+          (?E . 'all-the-icons-blue))))
 
   ;; LaTeX syntax highlighing
   (setq org-highlight-latex-and-related '(native script entities))
@@ -208,8 +121,8 @@
   (use-package! helm-bibtex
     :custom
     ;; In the lines below I point helm-bibtex to my default library file.
-    (bibtex-completion-bibliography '("~/Documents/.references/refs.bib"))
-    (reftex-default-bibliography '("~/Documents/.references/refs.bib"))
+    ;; (bibtex-completion-bibliography '("~/Documents/.references/refs.bib"))
+    ;; (reftex-default-bibliography '("~/Documents/.references/refs.bib"))
     ;; The line below tells helm-bibtex to find the path to the pdf
     ;; in the "file" field in the .bib file.
     (bibtex-completion-pdf-field "file")
@@ -224,16 +137,19 @@
   (map! :map helm-map
         "C-j" #'helm-next-line
         "C-k" #'helm-previous-line)
-)
+  (map! :mode org-mode
+        :leader
+        :desc "References"
+        "R i" #'org-ref-cite-insert-helm))
 
 ;; Set up org-ref stuff
 (use-package! org-ref
     :custom
     ;; Again, we can set the default library
-    (org-ref-default-bibliography "~/Documents/.references/refs.bib")
+    ;; (org-ref-default-bibliography "~/Documents/.references/refs.bib")
     ;; The default citation type of org-ref is cite:, but I use citep: much more often
     ;; I therefore changed the default type to the latter.
-    (org-ref-default-citation-link "citep")
+    (org-ref-default-citation-link "parencite")
  ;; The function below allows me to consult the pdf of the citation I currently have my cursor on.
  (defun my/org-ref-open-pdf-at-point ()
   "Open the pdf for bibtex key under point if it exists."
@@ -243,8 +159,8 @@
          (pdf-file (funcall org-ref-get-pdf-filename-function key)))
     (if (file-exists-p pdf-file)
         (find-file pdf-file)
-      (message "No PDF found for %s" key))))
-)
+      (message "No PDF found for %s" key)))))
+
   (setq org-ref-completion-library 'org-ref-ivy-cite
         org-export-latex-format-toc-function 'org-export-latex-no-toc
         org-ref-get-pdf-filename-function
@@ -252,22 +168,78 @@
         ;; See the function I defined above.
         org-ref-open-pdf-function 'my/org-ref-open-pdf-at-point
         ;; For pdf export engines.
-        org-latex-pdf-process (list "latexmk -pdflatex='%latex -shell-escape -interaction nonstopmode' -pdf -bibtex -f -output-directory=%o %f")
+        ;; org-latex-pdf-process (list "latexmk -pdflatex='%latex -shell-escape -interaction nonstopmode' -pdf -bibtex -f -output-directory=%o %f")
+        org-latex-pdf-process (list "latexmk -pdflatex=xelatex -shell-escape -bibtex -f -pdf %f")
         ;; I use orb to link org-ref, helm-bibtex and org-noter together (see below for more on org-noter and orb).
         org-ref-notes-function 'orb-edit-notes)
 
 (after! ox-latex
   (add-to-list 'org-latex-classes
-               '("apa6"
-                 "\\documentclass{apa6}"
+               '("appa"
+                 "\\documentclass[man]{apa7}
+                  \\usepackage[american]{babel}
+
+                  \\usepackage[backend=biber,style=apa]{biblatex} "
+
+                 ("\\section{%s}" . "\\section*{%s}")
+                 ("\\subsection{%s}" . "\\subsection*{%s}")
+                 ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+                 ("\\paragraph{%s}" . "\\paragraph*{%s}")
+                 ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
+  (add-to-list 'org-latex-classes
+               '("paper"
+               "\\documentclass[11pt]{article}
+                \\usepackage[utf8]{inputenc}
+                \\usepackage{hyperref}
+                \\usepackage{setspace}
+                \\usepackage{palatino}
+                \\usepackage{graphicx}
+                \\usepackage{float}
+                \\usepackage{titling} % drop vertical space before the title
+                \\usepackage{multirow}
+                \\usepackage{lscape}
+                \\usepackage{amsmath}
+                \\usepackage{amssymb}
+                \\usepackage{subcaption}
+                \\usepackage[a4paper, total={6in, 9.5in}]{geometry}
+                \\fontfamily{ppl}\\selectfont
+
+                \\usepackage[american]{babel}
+                \\usepackage[backend=biber, style=apa]{biblatex}
+
+                \\usepackage{setspace}
+                \\renewcommand{\\baselinestretch}{1.5}"
+
+                 ("\\section{%s}" . "\\section*{%s}")
+                 ("\\subsection{%s}" . "\\subsection*{%s}")
+                 ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+                 ("\\paragraph{%s}" . "\\paragraph*{%s}")
+                 ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
+  (add-to-list 'org-latex-classes
+               '("tudelft_basic"
+                 "\\documentclass[english]{article}
+                 \\usepackage{geometry}
+                 \\geometry{verbose,tmargin=3cm,bmargin=3cm,lmargin=3cm,rmargin=3cm}
+                 \\makeatletter
+                 \\usepackage{url}
+                 \\makeatother
+                 \\usepackage[american]{babel}
+                 \\usepackage[backend=biber, style=apa]{biblatex}"
                  ("\\section{%s}" . "\\section*{%s}")
                  ("\\subsection{%s}" . "\\subsection*{%s}")
                  ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
                  ("\\paragraph{%s}" . "\\paragraph*{%s}")
                  ("\\subparagraph{%s}" . "\\subparagraph*{%s}"))))
 
-(setq openwith-associations '(
-  ("\\.pdf\\'" "evince" (file))))
+(eval-after-load "org"
+  '(progn
+     ;; .txt files aren't in the list initially, but in case that changes
+     ;; in a future version of org, use if to avoid errors
+     (if (assoc "\\.txt\\'" org-file-apps)
+         (setcdr (assoc "\\.txt\\'" org-file-apps) "emacs %s")
+       (add-to-list 'org-file-apps '("\\.txt\\'" . "emacs %s") t))
+     ;; Change .pdf association directly within the alist
+     (setcdr (assoc "\\.pdf\\'" org-file-apps) "evince %s")))
 
 ;; ---- DART ----
 
@@ -394,7 +366,8 @@
            #'turn-on-auto-fill)
 
 (add-hook! 'org-mode-hook
-           #'turn-on-auto-fill)
+           #'turn-on-auto-fill
+           (visual-line-mode 0))
 
 (add-hook! 'emacs-startup-hook
            #'keychain-refresh-environment
